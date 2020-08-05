d3122ef690a8107decc116ccfe31c07eca28352e
almiao <foreverlee111@gmail.com>
2020-08-05 22:38:10 +0800

    'first'

--- dbblog-backend/dbblog-auth/src/main/java/cn/dblearn/blog/auth/controller/SysLoginController.java
+++ dbblog-backend/dbblog-auth/src/main/java/cn/dblearn/blog/auth/controller/SysLoginController.java
@@ -58,23 +58,23 @@ public class SysLoginController extends AbstractController {
 
     @PostMapping("/admin/sys/login")
     public Result login(@RequestBody SysLoginForm form) {
-        boolean captcha=sysCaptchaService.validate(form.getUuid(),form.getCaptcha());
-        if(!captcha){
-            // 验证码不正确
-            return Result.error(ErrorEnum.CAPTCHA_WRONG);
-        }
+//        boolean captcha=sysCaptchaService.validate(form.getUuid(),form.getCaptcha());
+//        if(!captcha){
+//            // 验证码不正确
+//            return Result.error(ErrorEnum.CAPTCHA_WRONG);
+//        }
 
         // 用户信息
         SysUser user = sysUserMapper.selectOne(new QueryWrapper<SysUser>()
                 .lambda()
                 .eq(SysUser :: getUsername,form.getUsername()));
-        if(user ==null || !user.getPassword().equals(new Sha256Hash(form.getPassword(),user.getSalt()).toHex())){
-            // 用户名或密码错误
-            return Result.error(ErrorEnum.USERNAME_OR_PASSWORD_WRONG);
-        }
-        if(user.getStatus() ==0){
-            return Result.error("账号已被锁定，请联系管理员");
-        }
+//        if(user ==null || !user.getPassword().equals(new Sha256Hash(form.getPassword(),user.getSalt()).toHex())){
+//            // 用户名或密码错误
+//            return Result.error(ErrorEnum.USERNAME_OR_PASSWORD_WRONG);
+//        }
+//        if(user.getStatus() ==0){
+//            return Result.error("账号已被锁定，请联系管理员");
+//        }
 
         //生成token，并保存到redis
         return sysUserTokenService.createToken(user.getUserId());
